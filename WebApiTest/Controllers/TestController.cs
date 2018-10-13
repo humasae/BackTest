d4a66c0e0df8740622c8b47a78c470bc5e0b3814
humasae <humasaenz@gmail.com>
2018-10-13 17:58:34 +0200

    Initial commit. Web Api basic authentication

---
+++ WebApiTest/Controllers/TestController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.Web.Http;
+using WebApiTest.Filters;
+
+namespace WebApiTest.Controllers
+{
+    /// <summary>
+    /// customer controller class for testing api
+    /// </summary>
+    [BasicAuthentication]
+    public class TestController : ApiController
+    {
+        // GET api/test 
+        public IEnumerable<string> Get()
+        {
+            return new[] { "value1", "value2" };
+        }
+
+        // GET api/test/5  
+        public string Get(int id)
+        {
+            return "value";
+        }
+    }
+}
